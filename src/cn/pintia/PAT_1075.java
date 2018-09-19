814fecd8270b583d8c41d63c107de464f795d84d
Liu ZS <lzfuzi@foxmail.com>
2018-09-19 11:18:10 +0800

    add 1075

--- src/cn/pintia/PAT_1075.java
+++ src/cn/pintia/PAT_1075.java
@@ -1,9 +1,17 @@
 package cn.pintia;
-
+/**
+ * 测试点2：
+ * 		00000 1 1
+ *		00000 1 -1
+ *
+ *		00000 2 1
+ *		00000 1 00001
+ *		00001 2 -1
+ *
+ *
+ */
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
 import java.util.Scanner;
 
 public class PAT_1075 {
@@ -15,101 +23,93 @@ public class PAT_1075 {
 		String startAddress = strings[0];
 		int N = Integer.parseInt(strings[1]);
 		int K = Integer.parseInt(strings[2]);
-		Map<String, Integer> map = new HashMap<>();
-		Element[] element = new Element[N];
+		new HashMap<>();
+		Element[] element = new Element[100000];
 		for(int i=0;i<N;i++){
 			String[] stringTemp = scanner.nextLine().split(" ");
-			map.put(stringTemp[0],Integer.parseInt(stringTemp[1]));
-			element[i].setAddress(stringTemp[0]);
-			
+			element[Integer.parseInt(stringTemp[0])] = new Element();
+			element[Integer.parseInt(stringTemp[0])].setData(Integer.parseInt(stringTemp[1]));
+			element[Integer.parseInt(stringTemp[0])].setNext(stringTemp[2]);			
 		}
-		
-		ArrayList<Element> list1 = new ArrayList<>();
-		ArrayList<Element> list2 = new ArrayList<>();
-		ArrayList<Element> list3 = new ArrayList<>();
-		
-		int l1 = 0;
-		boolean isl1 = true;
-		int ll1 = 0;
-		int l2 = 0;
-		int ll2 = 0;
-		boolean isl2 = true;
-		
-		for(int i=0;i<N;i++){
-			Element
-			if (map.get(startAddress) == K) {
-				list2.add(e);
-			}else{
-				if (e.getData() < K) {
-					if ((isl1)&(e.getData() >= 0)) {
-						list1.add(e);
-						l1 = ll1;
-						isl1 = false;
-					}else if ((!isl1)&(e.getData()>=0)) {
-						list1.add(e);
-					}else if (e.getData() < 0) {
-						if (!isl1) {
-							list1.add(l1, e);
-							l1++;
-						}else{
-							list1.add(e);
-						}
-					}	
-				}else if (e.getData() > K) {
-					if ((isl2)&(e.getData() >= 0)) {
-						list2.add(e);
-						l2 = ll2;
-						isl2 = false;
-					}else if ((!isl2)&(e.getData() >= 0)) {
-						list2.add(e);
-					}else if (e.getData() < 0) {
-						if (!isl2) {
-							list2.add(l2, e);
-							l2++;
-						}else{
-							list2.add(e);
-						}
-					}	
+		ArrayList<ArrayList<String>> list = new ArrayList<>();
+		ArrayList<String> list1 = new ArrayList<>();
+		ArrayList<String> list2 = new ArrayList<>();
+		ArrayList<String> list3 = new ArrayList<>();
+		list.add(list1);
+		list.add(list2);
+		list.add(list3);
+		while(!startAddress.equals("-1")){
+			int temp = element[Integer.parseInt(startAddress)].getData();
+			if (temp < 0) {
+				list1.add(startAddress);
+			}else if ((temp>=0)&&(temp<=K)) {
+				list2.add(startAddress);
+			}else {
+				list3.add(startAddress);
+			}
+			startAddress = element[Integer.parseInt(startAddress)].getNext();
+		}
+		int flag = 0;
+		for(int i = 0;i<3;i++){
+			for(int j=0;j<list.get(i).size();j++){
+				if (flag == 0) {
+					System.out.printf("%s %d ",list.get(i).get(j),element[Integer.parseInt(list.get(i).get(j))].getData());
+					flag = 1;
+				}else {
+					System.out.printf("%s%n%s %d ",list.get(i).get(j),list.get(i).get(j),element[Integer.parseInt(list.get(i).get(j))].getData());
 				}
 			}
 		}
-		String addressT = "";
-		for(int i=0;i<list1.size();i++){
-			if (i != list1.size()) {
-				System.out.println(list1.get(i).getAddress()+" "+list1.get(i).getData()+" "+list1.get(i+1).getAddress());
-			}else{
-				System.out.println(list1.get(i).getAddress()+" "+list1.get(i).getData()+" "+list2.get(0).getAddress());
-			}
-		}
-		if (list3.get(0)!=null) {
-			System.out.println(list2.get(0).getAddress()+" "+list2.get(0).getData()+" "+list3.get(0).getAddress());
-			for(int i=0;i<list3.size();i++){
-				if (i != list3.size()) {
-					System.out.println(list3.get(i).getAddress()+" "+list3.get(i).getData()+" "+list3.get(i+1).getAddress());
-				}else{
-					System.out.println(list3.get(i).getAddress()+" "+list3.get(i).getData()+" -1");
-				}
-			}
-		}else{
-			System.out.println(list2.get(0).getAddress()+" "+list2.get(0).getData()+" -1");
-		}
-		
-		
+		System.out.println("-1");
+//		if (list1.size()==1) {
+//			System.out.println(list1.get(list1.size()-1)+" "+element[Integer.parseInt(list1.get(list1.size()-1))].getData()+" "+list2.get(0));
+//		}else if (list1.size()>1) {
+//			for (int i = 0; i < list1.size()-1; i++) {
+//				System.out.println(list1.get(i)+" "+element[Integer.parseInt(list1.get(i))].getData()+" "+list1.get(i+1));
+//			}
+//			System.out.println(list1.get(list1.size()-1)+" "+element[Integer.parseInt(list1.get(list1.size()-1))].getData()+" "+list2.get(0));
+//		}
+//		if (list2.size() == 1) {
+//			if (list3.size()>0) {
+//				System.out.println(list2.get(list2.size()-1)+" "+element[Integer.parseInt(list2.get(list2.size()-1))].getData()+" "+list3.get(0));
+//				if (list3.size() == 1) {
+//					System.out.println(list3.get(list3.size()-1)+" "+element[Integer.parseInt(list3.get(list3.size()-1))].getData()+" -1");
+//				}else if (list3.size()>1) {
+//					for (int i = 0; i < list3.size()-1; i++) {
+//						System.out.println(list3.get(i)+" "+element[Integer.parseInt(list3.get(i))].getData()+" "+list3.get(i+1));
+//					}
+//					System.out.println(list3.get(list3.size()-1)+" "+element[Integer.parseInt(list3.get(list3.size()-1))].getData()+" -1");
+//				}
+//			}else {
+//				System.out.println(list2.get(list2.size()-1)+" "+element[Integer.parseInt(list2.get(list2.size()-1))].getData()+" -1");
+//			}
+//		}else if (list2.size() > 1) {
+//			for (int i = 0; i < list2.size()-1; i++) {
+//				System.out.println(list2.get(i)+" "+element[Integer.parseInt(list2.get(i))].getData()+" "+list2.get(i+1));
+//			}
+//			if (list3.size()>0) {
+//				System.out.println(list2.get(list2.size()-1)+" "+element[Integer.parseInt(list2.get(list2.size()-1))].getData()+" "+list3.get(0));
+//				if (list3.size() == 1) {
+//					System.out.println(list3.get(list3.size()-1)+" "+element[Integer.parseInt(list3.get(list3.size()-1))].getData()+" -1");
+//				}else if (list3.size()>1) {
+//					for (int i = 0; i < list3.size()-1; i++) {
+//						System.out.println(list3.get(i)+" "+element[Integer.parseInt(list3.get(i))].getData()+" "+list3.get(i+1));
+//					}
+//					System.out.println(list3.get(list3.size()-1)+" "+element[Integer.parseInt(list3.get(list3.size()-1))].getData()+" -1");
+//				}
+//			}else {
+//				System.out.println(list2.get(list2.size()-1)+" "+element[Integer.parseInt(list2.get(list2.size()-1))].getData()+" -1");
+//			}
+//		}
+
 	}
 
 }
 class Element{
-	private String address;
 	private int data;
-	private Element next;
+	private String next;
 	public Element(){
 		
-	}
-	public String getAddress() {
-		return address;
-	}
-	public void setAddress(String address) {
-		this.address = address;
 	}
 	public int getData() {
 		return data;
@@ -117,10 +117,10 @@ class Element{
 	public void setData(int data) {
 		this.data = data;
 	}
-	public Element getNext() {
+	public String getNext() {
 		return next;
 	}
-	public void setNext(Element next) {
+	public void setNext(String next) {
 		this.next = next;
 	}
 	
